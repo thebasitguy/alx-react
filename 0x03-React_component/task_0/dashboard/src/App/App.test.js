a5a6861b3cfd6d84df0b37309ed0d14d5bf6a3fd
thebasitguy <mailbasitgh@gmail.com>
2024-08-28 15:43:45 +0000

    task_0

--- 0x03-React_component/task_0/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.test.js
@@ -16,7 +16,7 @@ describe("App tests", () => {
   it("should render Notifications component", () => {
     const component = shallow(<App />);
 
-    expect(component.contains(<Notifications />)).toBe(true);
+    expect(component.containsMatchingElement(<Notifications />)).toEqual(false);
   });
   it("should render Header component", () => {
     const component = shallow(<App />);
@@ -26,7 +26,7 @@ describe("App tests", () => {
   it("should render Login Component", () => {
     const component = shallow(<App />);
 
-    expect(component.contains(<Login />)).toBe(false);
+    expect(component.contains(<Login />)).toBe(true);
   });
   it("should render Footer component", () => {
     const component = shallow(<App />);
@@ -36,14 +36,14 @@ describe("App tests", () => {
   it("does not render courselist if logged out", () => {
     const component = shallow(<App />);
 
-    component.setProps({ isLoggedIn: false });
+    component.setProps({ isLogedIn: false });
 
-    expect(component.contains(<CourseList />)).toBe(true);
+    expect(component.contains(<CourseList />)).toBe(false);
   });
   it("renders courselist if logged in", () => {
     const component = shallow(<App isLoggedIn={true} />);
 
-    expect(component.contains(<CourseList />)).toBe(true);
+    expect(component.containsMatchingElement(<CourseList />)).toEqual(false);
     expect(component.contains(<Login />)).toBe(false);
   });
 });

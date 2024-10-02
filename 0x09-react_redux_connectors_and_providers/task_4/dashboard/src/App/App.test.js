cecafeabe19386710bef71ef46051a5f0248fcd0
thebasitguy <mailbasitgh@gmail.com>
2024-10-02 23:43:00 +0000

    task_4

--- 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/App/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/App/App.test.js
@@ -76,18 +76,22 @@ describe("<App />", () => {
 
 describe("App Redux", () => {
   it("mapStateToProps returns the right object from user Login", () => {
-    let state = fromJS({
-      isUserLoggedIn: true,
-    });
+    let state = {
+      ui: fromJS({
+        isUserLoggedIn: true,
+      }),
+    };
 
     const result = mapStateToProps(state);
 
     expect(result).toEqual({ isLoggedIn: true });
   });
   it("mapStateToProps returns the right object from display Drawer", () => {
-    let state = fromJS({
-      isNotificationDrawerVisible: true,
-    });
+    let state = {
+      ui: fromJS({
+        isNotificationDrawerVisible: true,
+      }),
+    };
 
     const result = mapStateToProps(state);
 

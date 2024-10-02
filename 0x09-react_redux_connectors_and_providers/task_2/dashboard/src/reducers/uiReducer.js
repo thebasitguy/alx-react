ea57b05d1fcd191405f54ea2afe46194a61fdcb2
thebasitguy <mailbasitgh@gmail.com>
2024-10-02 15:23:06 +0000

    task_2

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
@@ -1,40 +1,45 @@
-import { Map } from 'immutable';
+import { Map } from "immutable";
+import { logout } from "../actions/uiActionCreators";
+
 import {
-  LOGIN,
-  LOGOUT,
-  DISPLAY_NOTIFICATION_DRAWER,
-  HIDE_NOTIFICATION_DRAWER,
-  LOGIN_SUCCESS,
-  LOGIN_FAILURE,
-} from '../actions/uiActionTypes';
+    LOGIN,
+    LOGOUT,
+    DISPLAY_NOTIFICATION_DRAWER,
+    HIDE_NOTIFICATION_DRAWER,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+} from "../actions/uiActionTypes";
 
 export const initialState = {
-  isNotificationDrawerVisible: false,
-  isUserLoggedIn: false,
-  user: {},
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: false,
+    user: null,
 };
 
 const uiReducer = (state = Map(initialState), action) => {
-  switch (action.type) {
-    case DISPLAY_NOTIFICATION_DRAWER:
-      return state.set('isNotificationDrawerVisible', true);
+    switch (action.type) {
+        case DISPLAY_NOTIFICATION_DRAWER:
+            return state.set("isNotificationDrawerVisible", true);
 
-    case HIDE_NOTIFICATION_DRAWER:
-      return state.set('isNotificationDrawerVisible', false);
+        case HIDE_NOTIFICATION_DRAWER:
+            return state.set("isNotificationDrawerVisible", false);
 
-    case LOGIN_SUCCESS:
-      return state.set('isUserLoggedIn', true);
+        case LOGIN_SUCCESS:
+            return state.set("isUserLoggedIn", true);
 
-    case LOGIN_FAILURE:
-      return state.set('isUserLoggedIn', false);
+        case LOGIN_FAILURE:
+            return state.set("isUserLoggedIn", false);
 
-    case LOGOUT:
-      return state.set('isUserLoggedIn', false);
+        case LOGIN:
+            return state.set("user", action.user);
 
-    default:
-      break;
-  }
-  return state;
+        case LOGOUT:
+            return state.set("isUserLoggedIn", false).set("user", null);
+
+        default:
+            break;
+    }
+    return state;
 };
 
 export default uiReducer;
